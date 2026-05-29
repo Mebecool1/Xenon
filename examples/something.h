c961204c5aade14b951e30295f922b108fb8036b
computergod47-png <computergod47@gmail.com>
2026-05-29 23:22:13 +0530

    better safety and borrow checking, more warnings instead of hard errors, -help flag, partial Option type.

---
+++ examples/something.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <stdio.h>
+void printSome() {
+    printf("Hello world!\n");
+}
+typedef struct vec3 {
+    float x;
+    float y;
+    float z;
+} vec3;
+typedef struct v2 {
+    float x;
+    float y;
+} v2; 
